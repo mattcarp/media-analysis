fae2e91733940356eca3fb8f1353d83aa79d82fd
mattcarp <matt@mattcarpenter.com>
2016-02-04 15:00:38 +0000

    enable cors, rewrite black detection in es6

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
-var express = require('express');
+const express = require('express');
 var http = require('http');
 var path = require('path');
 var favicon = require('serve-favicon');
@@ -12,9 +12,9 @@ var routes = require('./routes/index');
 var users = require('./routes/users');
 var analysis = require('./routes/media-analysis');
 var black = require('./routes/black');
-var mono = require('./routes/mono');
+const mono = require('./routes/mono');
 
-var app = express();
+const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'client'));
@@ -25,14 +25,22 @@ app.set('view engine', 'html');
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 
-app.use(bodyParser.raw(
-{limit: '50mb'}
-));
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+app.use(bodyParser.raw({
+  limit: "50mb"
+}));
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.use('/', routes);
 app.use('/users', users);
@@ -41,8 +49,8 @@ app.use('/black', black);
 app.use('/mono', black);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
+app.use(function (req, res, next) {
+  var err = new Error("Not Found");
   err.status = 404;
   next(err);
 });
@@ -51,8 +59,8 @@ app.use(function(req, res, next) {
 
 // development error handler
 // will print stacktrace
-if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
+if(app.get('env') === 'development') {
+  app.use(function (err, req, res, next) {
     res.status(err.status || 500);
     res.render('error', {
       message: err.message,
@@ -63,7 +71,7 @@ if (app.get('env') === 'development') {
 
 // production error handler
 // no stacktraces leaked to user
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   res.status(err.status || 500);
   res.render('error', {
     message: err.message,
@@ -71,6 +79,5 @@ app.use(function(err, req, res, next) {
     error: err
   });
 });
-
 
 module.exports = app;
--- routes/black.js
+++ routes/black.js
@@ -1,86 +1,75 @@
-var express = require('express');
-var router = express.Router();
+/* eslint no-console: 0 */
 
-/* post file slice for black detection. */
-// router.post('/', function(req, res, next) {
-//   res.send('whe should perform black detection at this point');
-// });
+const express = require("express");
+const router = express.Router();
+const fs = require("fs");
+const stream = require("stream");
+const exec = require("child_process").exec;
+// const child;
+// let result;
 
 module.exports = router;
-var express = require( 'express' );
-var router = express.Router();
-var ffmpeg = require( 'fluent-ffmpeg' );
-var fs = require( 'fs' );
-var binary = require( 'binary' );
-var bodyParser = require('body-parser');
-var stream = require( 'stream' );
-var randomstring = require("randomstring");
-var exec = require( 'child_process' ).exec,
-    child;
-var result;
 
-router.post('/', function(req, res, next) {
-    "use strict";
+router.post("/", (req, res) => {
+  const randomstring = require("randomstring").generate(12);
+  const bufferStream = new stream.PassThrough();
+  bufferStream.end(req.body);
+  console.log("you hit the black detection endpoint with a POST");
+  console.log("content type:");
+  console.log(req.headers["content-type"]);
 
-    var bufferStream = new stream.PassThrough();
-    bufferStream.end(req.body);
-    console.log("you hit the black detection endpoint with a POST");
-    console.log("content type:");
-    console.log(req.headers['content-type']);
+  console.log("req.body and length:");
+  console.log(req.body);
+  console.log(req.body.length);
+  const prefix = "/tmp/";
+  const tempName = prefix + randomstring;
+  console.log("the temp name:");
+  console.log(tempName);
 
-    console.log("req.body and length:");
-    console.log(req.body);
-    console.log(req.body.length);
+  fs.writeFile(tempName, req.body, err => {
+    if (err) {
+      return console.log(err);
+    }
 
-    var tempName = "/tmp/" + randomstring.generate(12);
-    console.log("the temp name:");
-    console.log(tempName);
+    console.log("call ffprobe black detection:");
+    const ffprobeCmd = `ffprobe -f lavfi -i "movie=${tempName},blackdetect[out0]"` +
+      ` -show_entries tags=lavfi.black_start,lavfi.black_end -of default=nw=1 -v quiet`;
 
-    fs.writeFile( tempName, req.body, function( err ) {
-        if ( err ) {
-            return console.log( err );
+    exec(ffprobeCmd,
+      (error, stdout, stderr) => {
+        const result = {};
+        console.log("STDOUT:", stdout);
+        console.log("STDERR:", stderr);
+        if (error !== null) {
+          console.log("here is the exec error from ffprobe: ", error);
         }
+        // TODOmc filter empty elements
+        const analysisArr = stdout.split("\n").filter(item => item !== "");
+        console.log("my analysisArr");
+        console.log(analysisArr);
 
-        console.log("call ffprobe black detection:");
-        let ffprobeCmd = `ffprobe -f lavfi -i "movie=${tempName},blackdetect[out0]"` +
-        ` -show_entries tags=lavfi.black_start,lavfi.black_end -of default=nw=1 -v quiet`
+        const blackObjs = analysisArr.map(item => {
+          const temp = {};
+          if (item.indexOf("black_start") > -1) {
+            temp.tag = "blackStart";
+          }
+          if (item.indexOf("black_end") > -1) {
+            temp.tag = "blackEnd";
+          }
+          temp.value = item.substr(item.indexOf("=") + 1);
+          return temp;
+        });
+        // TODOmc take blackObjs array and transfomr to 'blackIntervals'
+        // arr, with start, end, and duration
+        console.log("my black objs:");
+        console.log(blackObjs);
+        result.error = stderr;
+        result.blackDetect = blackObjs;
+        res.json(result);
+      });
 
-        child = exec(ffprobeCmd,
-            function(error, stdout, stderr) {
-
-                let result = {}
-                console.log("STDOUT: " + stdout );
-                console.log("STDERR: " + stderr );
-                if ( error !== null ) {
-                  console.log("here is the exec error from ffprobe: " + error );
-                }
-                // TODOmc filter empty elements
-                let analysisArr = stdout.split("\n").filter(item => item !== "");
-                console.log("my analysisArr");
-                console.log(analysisArr);
-
-                let blackObjs = analysisArr.map(item => {
-                  let temp = {};
-                  if (item.indexOf("black_start") > -1) {
-                    temp.tag = "blackStart";
-                  }
-                  if (item.indexOf("black_end") > -1 ) {
-                    temp.tag = "blackEnd";
-                  }
-                  temp.value = item.substr(item.indexOf("=") + 1);
-                  return temp;
-                });
-                // TODOmc take blackObjs array and transfomr to 'blackIntervals'
-                // arr, with start, end, and duration
-                console.log("my black objs:");
-                console.log(blackObjs);
-                result.error = stderr;
-                result.blackDetect = blackObjs;
-                res.json(result);
-            } );
-
-        console.log("the temp file was saved");
-    } ); // writeFile
-} );
+    console.log("the temp file was saved");
+  }); // writeFile
+});
 
 module.exports = router;
