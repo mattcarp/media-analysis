54420e11583fa0c96b68c823387e215e728d40eb
Sergei <sergei.nikonov.intimetechnologies@sonymusic.com>
2020-07-28 15:18:29 +0300

    audio validation errors fixed

--- client/src/app/validate-format/validate-format.component.ts
+++ client/src/app/validate-format/validate-format.component.ts
@@ -55,37 +55,37 @@ export class ValidateFormatComponent {
 
     this.audioValidations.push({
       name: "Bit Depth",
-      value: audioStream.bits_per_sample,
-      pass: ACCEPTED_BIT_DEPTHS.indexOf(audioStream.bits_per_sample) > - 1,
+      value: audioStream && audioStream.bits_per_sample,
+      pass: ACCEPTED_BIT_DEPTHS.indexOf(audioStream && audioStream.bits_per_sample) > - 1,
       message: "Audio bit depth must be 16 bits."
     });
 
     this.audioValidations.push({
       name: "Sample Rate",
-      value: audioStream.sample_rate,
-      pass: audioStream.sample_rate === "48000",
+      value: audioStream && audioStream.sample_rate,
+      pass: audioStream && audioStream.sample_rate === "48000",
       message: "Audio sample rate must be 48kHz."
     });
 
     this.audioValidations.push({
       name: "Channel Layout",
-      value: audioStream.channel_layout,
-      pass: audioStream.channel_layout === "stereo",
+      value: audioStream && audioStream.channel_layout,
+      pass: audioStream && audioStream.channel_layout === "stereo",
       message: "Audio channel layout must be stereo."
     });
 
     this.audioValidations.push({
       name: "Channel Count",
-      value: audioStream.channels,
-      pass: audioStream.channels === 2,
+      value: audioStream && audioStream.channels,
+      pass: audioStream && audioStream.channels === 2,
       message: "There must be 2 audio channels."
     });
 
     this.audioValidations.push({
       name: "Codec",
-      value: audioStream.codec_long_name,
+      value: audioStream && audioStream.codec_long_name,
       // pass: audioStream.codec_long_name === "PCM signed 16-bit big-endian",
-      pass: ACCEPTED_AUDIO_CODECS.indexOf(audioStream.codec_name) > - 1,
+      pass: ACCEPTED_AUDIO_CODECS.indexOf(audioStream && audioStream.codec_name) > - 1,
       message: "Audio must be either 16 bit PCM or AAC."
     });
 
