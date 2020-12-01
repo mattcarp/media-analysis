7fc40ff15e999c27d27588a7efaf2303e2806a57
Sergei <sergei.nikonov.intimetechnologies@sonymusic.com>
2020-12-01 12:45:59 +0200

    [AOMA-16303] sme-uploader route fixed

--- apps/backend/src/app/routes/sme-uploader.ts
+++ apps/backend/src/app/routes/sme-uploader.ts
@@ -2,11 +2,11 @@ import * as express from 'express';
 import * as path from 'path';
 
 const smeUploaderRouter = express.Router();
-// todo handle via libs
-smeUploaderRouter.get('*.*', express.static(path.join(__dirname, '/../../sme-uploader/demo/')));
+
+smeUploaderRouter.get('*.*', express.static(path.join(__dirname, '../sme-uploader/')));
 
 smeUploaderRouter.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '/../../sme-uploader/demo/index.html'));
+  res.sendFile(path.join(__dirname, '../sme-uploader/index.html'));
 });
 
 export { smeUploaderRouter };
